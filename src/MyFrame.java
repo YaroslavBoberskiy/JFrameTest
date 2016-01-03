3194f16b9ad8881a1ea5576c11dea6da1dc75e26
Yaroslav <yaroslav.boberskiy@gmail.com>
2016-01-03 18:43:55 +0200

    New frame with olympic panel

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,16 +1,35 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JPanel {
 
-    MyFrame(String title) {
-        this.setTitle(title);
-        this.setSize(800, 600);
-        this.setLocation(300, 100);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public MyFrame(){
+        JFrame frame = new JFrame("New Frame");
+        frame.setLocation(300, 300);
+        frame.setMinimumSize(new Dimension(500, 250));
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.GREEN);
+        g.drawOval(100, 50, 100, 100);
+        g.setColor(Color.RED);
+        g.drawOval(170, 50, 100, 100);
+        g.setColor(Color.BLACK);
+        g.drawOval(240, 50, 100, 100);
+        g.setColor(Color.YELLOW);
+        g.drawOval(130, 100, 100, 100);
+        g.setColor(Color.BLUE);
+        g.drawOval(200, 100, 100, 100);
     }
 
     public static void main(String[] args) {
-        MyFrame frame = new MyFrame("Window");
-        frame.setVisible(true);
+        MyFrame myFrame = new MyFrame();
+
     }
 }
